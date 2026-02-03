794879e7daeaa321f34c3faed6e9d350a005a7ef
genaisrini <genai.srini@gmail.com>
2026-02-03 22:31:17 +0530

    Update variables.tf

--- terraform/modules/vm/variables.tf
+++ terraform/modules/vm/variables.tf
@@ -1,1 +1,20 @@
+variable "vm_name" {}
+variable "location" {}
+variable "resource_group_name" {}
+variable "subnet_id" {}
+variable "vm_size" {}
 
+variable "os_type" {
+  description = "linux or windows"
+  type        = string
+}
+
+variable "admin_username" {}
+variable "ssh_public_key" {
+  default = ""
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
